ffb7481462bd146968d1b09b2ebef2089345acdb
mileslathan <mlathan.cn@gmail.com>
2022-07-24 16:08:31 -0400

    added my models to my project

--- core_app/models.py
+++ core_app/models.py
@@ -1,8 +1,30 @@
 from django.db import models
-  
+from django.contrib.auth.models import User
 # Create your models here.
   
   
 class React(models.Model):
     name = models.CharField(max_length=30)
     detail = models.CharField(max_length=500)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+
+class Collection(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    records = models.ManyToManyField(Record, on_delete=Models.CASCADE)
+
+class Record(models.Model):
+    name = models.CharField(max_length=100)
+    label = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    rec_format = models.CharField(max_length=50)
+    released = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    style = models.CharField(max_length=50)
+
+class Rating(models.Model):
+    rating = models.CharField()
