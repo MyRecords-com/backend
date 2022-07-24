7c3c5bb455a1b1964631a4a03c83be38cb7eaeed
mileslathan <mlathan.cn@gmail.com>
2022-07-24 17:50:25 -0400

    Tested Profile model and is properly associated with users.

--- core_app/admin.py
+++ core_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, Record
+
+#Register Your Models Here
+admin.site.register(Profile)
+admin.site.register(Record)
--- core_app/models.py
+++ core_app/models.py
@@ -9,7 +9,7 @@ class React(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    created_date = DateField(auto_now_add=True)
+    created_date = models.DateField(auto_now_add=True)
     location = models.CharField(max_length=100)
     usr_type = models.CharField(max_length=50)
     setup = models.TextField()
@@ -28,11 +28,11 @@ class Collection(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    records = models.ManyToManyField(Record, on_delete=Models.CASCADE)
+    records = models.ManyToManyField(Record)
 
 class Comment(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
-    record = models.ForeignKey(Record, on_delete=modles.CASCADE)
-    rating = models.CharField()
+    record = models.ForeignKey(Record, on_delete=models.CASCADE)
+    rating = models.CharField(max_length=20)
     comment = models.TextField()
