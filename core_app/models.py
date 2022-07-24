a9904a6aeeaed19896339e94fa5eda86b29964a7
mileslathan <mlathan.cn@gmail.com>
2022-07-24 16:53:26 -0400

    revision of models before migrations.

--- core_app/models.py
+++ core_app/models.py
@@ -9,17 +9,11 @@ class React(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    created_date = DateField()
+    created_date = DateField(auto_now_add=True)
     location = models.CharField(max_length=100)
     usr_type = models.CharField(max_length=50)
     setup = models.TextField()
     bio = models.TextField()
-
-class Collection(models.Model):
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    records = models.ManyToManyField(Record, on_delete=Models.CASCADE)
 
 class Record(models.Model):
     name = models.CharField(max_length=100)
@@ -30,9 +24,15 @@ class Record(models.Model):
     genre = models.CharField(max_length=50)
     style = models.CharField(max_length=50)
 
+class Collection(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    records = models.ManyToManyField(Record, on_delete=Models.CASCADE)
+
 class Comment(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    created_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
     record = models.ForeignKey(Record, on_delete=modles.CASCADE)
     rating = models.CharField()
     comment = models.TextField()
